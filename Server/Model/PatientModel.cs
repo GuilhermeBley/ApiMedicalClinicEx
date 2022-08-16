e953d99639e1f7c0537f349bab096f4e4490ffc4
GuilhermeBley <guilhermecbley@gmail.com>
2022-08-16 00:53:56 -0300

    Repair model patient CPF

--- Server/Model/PatientModel.cs
+++ Server/Model/PatientModel.cs
@@ -6,12 +6,7 @@ public class PatientModel
 {
     [Required]
     [RegularExpression(@"([0-9]{11})", ErrorMessage = "Only CPF numbers. Ex '00011122233'.")]
-    public string? Cpf { 
-        get { 
-            return $"{(Cpf is null ? "" : Cpf.SkipLast(4))}"; 
-        } 
-        set { Cpf = value; } 
-    }
+    public string? Cpf { get; set; }
 
     [Required]
     public string? Name { get; set; }
